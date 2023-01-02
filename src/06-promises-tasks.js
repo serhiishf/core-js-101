ab00502813ba8f82f540c9058cb36376aeeeaf7b
serhiishf <unison447@gmail.com>
2023-01-02 23:35:47 +0200

    feat: Done task from Promise

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -27,8 +27,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer) {
+      resolve('Hooray!!! She said "Yes"!');
+    } else if (isPositiveAnswer === false) {
+      resolve('Oh no, she said "No".');
+    }
+    reject(new Error('Wrong parameter is passed! Ask her again.'));
+  });
+  // throw new Error('Not implemented');
 }
 
 /**
